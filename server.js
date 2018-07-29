7619b7183756de2c28900137e3b5595e56208113
yuvalbl <yuval.bar.levi@gmail.com>
2018-07-29 18:49:28 +0300

    counter

--- server.js
+++ server.js
@@ -2,18 +2,28 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+let counter = 0;
 
 // serve client files
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
 
+
 io.on('connection', function(socket){
   console.log('a user connected');
   // get data from client
-  socket.on('some event', function(msg){
-    console.log('message: ' + msg);
+  socket.on('reset event', () =>{
+    console.log('reset ');
+    counter = 0;
   });
+
+
+  setInterval(() => {
+    counter++;
+    socket.emit('counter', counter);
+  }, 2000);
+
 });
 
 http.listen(3000, function(){
